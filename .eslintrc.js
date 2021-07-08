e32f66c6a3a2db11a9a3b280172848712c57474d
ahwgs <ah_wgs@126.com>
2021-07-08 22:20:21 +0800

    chore: update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,5 +6,8 @@ module.exports = {
   },
   rules: {
     'import/prefer-default-export': 0,
+    'import/extensions': 0,
+    'jsx-quotes': 0,
+    'unicorn/consistent-destructuring': 0,
   },
 }
--- src/components/chart/index.tsx
+++ src/components/chart/index.tsx
@@ -1,42 +1,35 @@
-import React, { useContext, useCallback, useEffect } from 'react';
-import { observer } from 'mobx-react-lite';
-import DragPresent from '../drag-present';
-import BarList from '../bar-list';
-import BarThumbList from '../bar-thumb-list';
-import Today from '../today';
-import Dependencies from '../dependencies';
-import Context from '../../context';
-import './index.less';
+import React, { useContext, useCallback, useEffect } from 'react'
+import { observer } from 'mobx-react-lite'
+import DragPresent from '../drag-present'
+import BarList from '../bar-list'
+import BarThumbList from '../bar-thumb-list'
+import Today from '../today'
+import Dependencies from '../dependencies'
+import Context from '../../context'
+import './index.less'
 
 const Chart: React.FC = () => {
-  const { store, prefixCls , disabled } = useContext(Context);
-  const {
-    tableWidth,
-    viewWidth,
-    bodyScrollHeight,
-    translateX,
-    chartElementRef,
-  } = store;
-  const minorList = store.getMinorList();
+  const { store, prefixCls } = useContext(Context)
+  const { tableWidth, viewWidth, bodyScrollHeight, translateX, chartElementRef } = store
+  const minorList = store.getMinorList()
   const handleMouseMove = useCallback(
     (event: React.MouseEvent<HTMLDivElement>) => {
-      event.persist();
-      store.handleMouseMove(event);
+      event.persist()
+      store.handleMouseMove(event)
     },
     [store]
-  );
+  )
   const handleMouseLeave = useCallback(() => {
-    store.handleMouseLeave();
-  }, [store]);
+    store.handleMouseLeave()
+  }, [store])
   useEffect(() => {
-    const element = chartElementRef.current;
-    if (element) {
-      element.addEventListener('wheel', store.handleWheel);
-    }
+    const element = chartElementRef.current
+    if (element) element.addEventListener('wheel', store.handleWheel)
+
     return () => {
-      element && element.removeEventListener('wheel', store.handleWheel);
-    };
-  }, [chartElementRef, store]);
+      if (element) element.removeEventListener('wheel', store.handleWheel)
+    }
+  }, [chartElementRef, store])
   return (
     <div
       ref={chartElementRef}
@@ -51,31 +44,31 @@ const Chart: React.FC = () => {
     >
       <svg
         className={`${prefixCls}-chart-svg-renderer`}
-        xmlns="http://www.w3.org/2000/svg"
-        version="1.1"
+        xmlns='http://www.w3.org/2000/svg'
+        version='1.1'
         width={viewWidth}
         height={bodyScrollHeight}
         viewBox={`${translateX} 0 ${viewWidth} ${bodyScrollHeight}`}
       >
         <defs>
           <pattern
-            id="repeat"
-            width="4.5"
-            height="10"
-            patternUnits="userSpaceOnUse"
-            patternTransform="rotate(70 50 50)"
+            id='repeat'
+            width='4.5'
+            height='10'
+            patternUnits='userSpaceOnUse'
+            patternTransform='rotate(70 50 50)'
           >
-            <line stroke="#c6c6c6" strokeWidth="1px" y2="10" />
+            <line stroke='#c6c6c6' strokeWidth='1px' y2='10' />
           </pattern>
         </defs>
-        {minorList.map((item) =>
+        {minorList.map(item =>
           item.isWeek ? (
-            <g key={item.key} stroke="#f0f0f0">
+            <g key={item.key} stroke='#f0f0f0'>
               <path d={`M${item.left},0 L${item.left},${bodyScrollHeight}`} />
               <rect
-                fill="url(#repeat)"
-                opacity="0.5"
-                strokeWidth="0"
+                fill='url(#repeat)'
+                opacity='0.5'
+                strokeWidth='0'
                 x={item.left}
                 y={0}
                 width={item.width}
@@ -83,7 +76,7 @@ const Chart: React.FC = () => {
               />
             </g>
           ) : (
-            <g key={item.key} stroke="#f0f0f0">
+            <g key={item.key} stroke='#f0f0f0'>
               <path d={`M${item.left},0 L${item.left},${bodyScrollHeight}`} />
             </g>
           )
@@ -103,6 +96,6 @@ const Chart: React.FC = () => {
         <Today />
       </div>
     </div>
-  );
-};
-export default observer(Chart);
+  )
+}
+export default observer(Chart)
